ff33c7bf4b95adcb76bfb7bee0db5478b0aa8071
Christoph Dörr <Christoph.DA.Doerr@bmw.de>
2021-05-03 14:56:41 +0200

    add echos for debbuging

--- .github/kubeval.sh
+++ .github/kubeval.sh
@@ -9,7 +9,11 @@ SCHEMA_LOCATION="https://raw.githubusercontent.com/instrumenta/kubernetes-json-s
 curl --silent --show-error --fail --location --output /tmp/kubeval.tar.gz https://github.com/instrumenta/kubeval/releases/download/"${KUBEVAL_VERSION}"/kubeval-linux-amd64.tar.gz
 tar -xf /tmp/kubeval.tar.gz kubeval
 
+ls -la
+echo $CHART_DIRS
+
 # validate charts
 for CHART_DIR in ${CHART_DIRS}; do
+  echo $CHART_DIR
   helm template "${CHART_DIR}" --debug | ./kubeval --ignore-missing-schemas --kubernetes-version "${KUBERNETES_VERSION#v}" --schema-location "${SCHEMA_LOCATION}"
 done
