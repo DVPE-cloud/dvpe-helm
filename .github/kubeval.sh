bfbb3949887359e8c33ce8e1efe551cf4b8bbfd1
Christoph Dörr <Christoph.DA.Doerr@bmw.de>
2021-05-03 15:48:49 +0200

    add more log outputs to kubeval

--- .github/kubeval.sh
+++ .github/kubeval.sh
@@ -1,29 +1,27 @@
 #!/bin/bash
 set -euo pipefail
 
-echo 'test'
-
-ls -la
-
 KUBEVAL_VERSION="v0.16.1"
-echo KUBEVAL_VERSION
-echo KUBERNETES_VERSION
+echo "Using Kubeval Version: ${KUBEVAL_VERSION}"
 
 SCHEMA_LOCATION="https://raw.githubusercontent.com/instrumenta/kubernetes-json-schema/master/"
-echo SCHEMA_LOCATION
+echo "Using Schema Location: ${SCHEMA_LOCATION}"
 
 CHART_DIRS="$(git diff --find-renames --name-only "$(git rev-parse --abbrev-ref HEAD)" remotes/origin/master -- charts | grep '[cC]hart.yaml' | sed -e 's#/[Cc]hart.yaml##g')"
-echo CHART_DIRS
+if [ -z "$CHART_DIRS" ]
+then
+      echo "Chart Directory is empty. Please check if you increased the Chart version."
+      exit 1
+fi
+echo "Using Chart Directory: ${!$CHART_DIRS}"
 
 # install kubeval
 curl --silent --show-error --fail --location --output /tmp/kubeval.tar.gz https://github.com/instrumenta/kubeval/releases/download/"${KUBEVAL_VERSION}"/kubeval-linux-amd64.tar.gz
 tar -xf /tmp/kubeval.tar.gz kubeval
 
-ls -la
-echo CHART_DIRS
+echo "Downloaded and unpacked kubeval successfully"
 
 # validate charts
 for CHART_DIR in ${CHART_DIRS}; do
-  echo CHART_DIR
   helm template "${CHART_DIR}" --debug | ./kubeval --ignore-missing-schemas --kubernetes-version "${KUBERNETES_VERSION#v}" --schema-location "${SCHEMA_LOCATION}"
 done
